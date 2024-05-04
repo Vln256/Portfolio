05a4dc39303b829a6f4c5baeb2dbad28d0b46d8b
Vln256 <147169803+Vln256@users.noreply.github.com>
2024-05-05 04:57:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-xmark");
+    navbar.classList.toggle('active');
+};
+
+/* scroll section active link */
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').includes(id)) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+};
+
+/* sticky navbar */
+window.addEventListener('scroll', () => {
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+});
+
+/* scroll reveal */
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/* typed js */
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Web Designer'],
+    typeSpeed: 70,
+    backspeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
